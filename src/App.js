de7d80fc12576cb14994f6033746c604c53b310f
ObeidSalem <o.salem322@gmail.com>
2021-07-15 14:38:26 +0530

    secconed commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React obeid
+          Learn React with Obeid and Abdi
         </a>
       </header>
     </div>
