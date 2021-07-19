eae29ea1987aba1bffe143956a417b917eb5e93a
ObeidSalem <o.salem322@gmail.com>
2021-07-19 11:06:40 +0530

    footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -9,24 +9,20 @@ const Footer = () => {
             <hr class="hr1" /> 
             <div className= " continer">
                 <div className="row" >
-                {/* colum1 */}
-                <div className="col1">
-                 <img className="footer-img"src={hadhrami_pic} alt="hadhrami-pic" width="50" height="80"/>
+                    {/* colum1 */}
+                    <div className="col1">
+                        <img className="footer-img"src={hadhrami_pic} alt="hadhrami-pic" width="50" height="80"/>
+                    </div>
+                    {/* colum2 */}
+                    <div className="col2">
+                        <ul clasName="list-unstyled">
+                            <li>copyright &copy;{new Date().getUTCFullYear()} Hadhrami Students CHapter, All Rights Reserved</li>
+                            <li>Nibrashs1@gmail.com</li>
+                            <li>hadhrami_iium</li>
+                        </ul>
+                    </div>
                 </div>
-                {/* colum2 */}
-                <div className="col2">
-                    <ul clasName="list-unstyled">
-                        <li>copyright &copy;{new Date().getUTCFullYear()} Hadhrami Students CHapter, All Rights Reserved</li>
-                        <li>Nibrashs1@gmail.com</li>
-                        <li>hadhrami_iium</li>
-                    
-                    </ul>
-                </div>
-
-                </div>
-
             </div>
-           
         </div>
     )
 }
