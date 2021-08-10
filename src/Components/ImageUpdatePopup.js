e153ae518af2360c800d807e26f10ae71c95c5fe
ObeidSalem <o.salem322@gmail.com>
2021-08-10 17:40:30 +0530

    Updates PopUp been added

---
+++ src/Components/ImageUpdatePopup.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Popup.css"
+
+const ImageUpdatePopup = (props) => {
+    return ( props.trigger) ? (
+        <div className="popup">
+            <div className="popup__container">
+                <button onClick={() => props.setTrigger(false)} className="close__button">CANCEL</button>
+                { props.children }
+            </div>
+        </div>
+    ) : "";
+}
+
+export default ImageUpdatePopup
